cfb445c9ede967f93d93ba7ef017bd3c96dfdafc
Ewen LE GUILLY <ewen.le-guilly@etu.univ-nantes.fr>
2024-10-19 12:04:37 +0200

    Add download of music clip from youtube

--- src/spotify.py
+++ src/spotify.py
@@ -4,8 +4,10 @@ import random
 import numpy as np
 from operator import itemgetter
 import sys
-import os
 from dotenv import load_dotenv
+from youtube import getClip
+from datetime import datetime
+import os
 
 def search_playlists_by_keyword(client, keywords):
     exclude_keywords = [
@@ -95,22 +97,29 @@ def main():
 
     CATEGORY_SIZE = int(sys.argv[1])
     MIN_POPULARITY = int(sys.argv[2])
-    CLIENT_ID = 'ae0e8df4f008454ba1db538f5ca59d78'
-    CLIENT_SECRET = 'f20fb86fc2ae475f8fa6295d9b8633ec'
+    KEYWORDS = " ".join(sys.argv[3:]) # We allow spaces for keywords (ex : for artists its easier => Ed Sheeran)
 
     # Configure authentication information
-    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("CLIENT_SECRET"))
 
     # Get the spotipy client
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
     # Use the function with exclusion of certain keywords
-    playlist = search_playlists_by_keyword(client=sp, keywords='blind test +2000')
+    playlist = search_playlists_by_keyword(client=sp, keywords=f" best {KEYWORDS}")
 
     # Retrieve the songs of the playlist
     songs = sp.playlist_items(playlist["id"])
 
-    selected_sounds = get_songs_by_category(songs['items'], MIN_POPULARITY, CATEGORY_SIZE)
+    selected_songs = get_songs_by_category(songs['items'], MIN_POPULARITY, CATEGORY_SIZE)
+
+    # Get the clip of each songs selected
+
+    directory = datetime.now().strftime("%d-%m-%Y")
+    for song in selected_songs:
+        print(song["name"])
+        print(song["artists"])
+        getClip(song["name"], song["artists"][0]["name"], directory)
 
 # Check if the script is executed directly (rather than being imported as a module)
 if __name__ == "__main__":
---
+++ src/youtube.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+import yt_dlp
+
+
+def getClip(title: str, artist: str, output_directory: str):
+    options = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': f'download_output/{output_directory}/{title} - {artist}.%(ext)s'
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        try:
+            ydl.download([f"ytsearch1:{title} {artist} music video official"])
+        except yt_dlp.utils.ExtractorError as e:
+            print(f"Error extracting information: {e}")
+        except yt_dlp.utils.DownloadError as e:
+            print(f"Error downloading video: {e}")
